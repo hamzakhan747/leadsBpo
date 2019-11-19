4f9aba2169262ac4b80af5218f7c7f34a87425e1
hamzakhan747 <mhamzakhan2019@gmail.com>
2019-11-20 03:49:52 +0500

    responsive2

--- src/component/footer/footer.js
+++ src/component/footer/footer.js
@@ -11,7 +11,7 @@ export default ()=>{
             <div className="col-md-12">
             <div className="footer-div1">
             <div className="pt-5"><img style={{width:"300px"}} src={img}/></div>
-            <div className="pt-5"><p>Copyright 2019 blackberryfinancesolutions.netlify.com All Rights Reserved.</p></div>
+            <div className="pt-5"><p className="footer-p1">Copyright 2019 blackberryfinancesolutions.netlify.com All Rights Reserved.</p></div>
             <div className="footer-div2 pt-4"><p>Blackberryfinancesolutions.netlify.com is a trading style of Money Tree Media Limited.
             </p>
             <p className="pt-4">We will never charge you any fees for using the services on this site whatsoever.
